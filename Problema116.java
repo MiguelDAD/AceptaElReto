dd39eb4e4747f091d77c5edbb55e36047199c852
MiguelDAD <75015762+MiguelDAD@users.noreply.github.com>
2022-04-27 18:25:37 +0200

    ¡Hola mundo!

---
+++ Problema116.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Problema116 {
+    public static void main(String[] args) {
+        Scanner popitas = new Scanner(System.in);
+
+        int n = popitas.nextInt(); 
+        for (int i = 0; i < n; i++) {
+            System.out.println("Hola mundo.");            
+        }
+    }
+}
